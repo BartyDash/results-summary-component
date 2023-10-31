108b7f1a674776de277cf8b37e2fbbbe2cf67891
BartyDash <writetobartek@gmail.com>
2023-10-31 19:06:45 +0100

    counter numbers animation

--- src/counter.js
+++ src/counter.js
@@ -11,7 +11,8 @@ for (const [index, element] of counters.entries()) {
     const icon = element.querySelector('.category__icon');
 
     category.textContent = data[index].category;
-    score.textContent = data[index].score;
+    score.textContent = 0;
+    updateCounter(score, data[index].score, 20);
     //icon.src = data[index].icon;//I don't know how to do it properly. it isn't working :(
     //icon.src = new URL('./assets/images/icon-reaction.svg', import.meta.url);
     //icon.src = new URL(data[0].icon, import.meta.url);
@@ -19,9 +20,27 @@ for (const [index, element] of counters.entries()) {
     fullScore += data[index].score;
 }
 //final result
-resultScore.textContent = parseInt(fullScore/counters.length);
+resultScore.textContent = 0;
+updateCounter(resultScore, parseInt(fullScore/counters.length), 20);
 
 //create new image in DOM
 // let img = document.createElement('img');
 // img.src = new URL('./assets/images/icon-reaction.svg', import.meta.url);
 // document.body.appendChild(img);
+
+
+function updateCounter(selector, targetScore, speed) {
+
+    const updateCount = () => {
+        const count = parseInt(selector.innerText);
+
+        if (count < targetScore) {
+            selector.innerText = count + 1;
+            setTimeout(updateCount, speed);
+        } else {
+            selector.innerText = targetScore;
+        }
+    }
+
+    updateCount();
+}
